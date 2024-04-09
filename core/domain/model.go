0bbb0a3d9641e1f23d80ba1be46fa0fbf0c6d335
Alejandro Waiz <alejosaurio1@gmail.com>
2024-04-09 13:30:58 -0400

    Adding basic pokemon struct

--- core/domain/model.go
+++ core/domain/model.go
@@ -1,1 +1,20 @@
 package domain
+
+//Struct representation of a pokemon created on excel
+type Pokemon struct {
+	ID                                                                                  int
+	Name, Types, Ability                                                                string
+	Stage                                                                               int
+	GrowRatio                                                                           string
+	TotalStats                                                                          int
+	Category, Nature                                                                    string
+	HpPhases, AttackPhases, DefensePhases, SpAttackPhases, SpDefensePhases, SpeedPhases string
+	//Each pokemon should have min 1 movement and max 4 movements
+	Movements []Movement
+}
+
+type Movement struct {
+	Name            string
+	Power, Accuracy int
+	MoveType, Class string
+}
